55452f4fef179986fe588d1bf6e13943338fdc3c
MXD <MXD789@GMAIL.COM>
2017-02-16 09:33:49 -0500

    webprogramming2 initial

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/20/5052d469dff20016102bab3315a73222
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<meta charset="UTF-8">
+<title></title>
+<body>
+	<h1>Welcome ${user}!</h1>
+	<#list 1..10 as i>
+		<p>Our latest product is :
+		${li} <a href="${latestProduct.url}">${latestProduct.name}</a>!<br/></p>
+	</#list>
+</body>
+</html>
---
+++ FreeMarker/src/edu/uga/cs/servlet/Product.java
@@ -0,0 +1,77 @@
+package edu.uga.cs.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Product
+ */
+@WebServlet("/Product")
+public class Product extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private String name; 
+	private String url; 
+	
+	public Product(String name, String url) {
+		this.name = name;
+		this.url  = url;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Product() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
