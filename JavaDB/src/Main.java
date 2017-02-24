9e308807c0a89aac5ca703a33aeccf8ab79d84e5
MXD <MXD789@GMAIL.COM>
2017-02-23 23:16:41 -0500

    continuing

--- JavaDB/src/Main.java
+++ JavaDB/src/Main.java
@@ -1,14 +1,39 @@
 import java.sql.DriverManager;
-import java.sql.*;
-import com.mysql.jdbc.Connection;
+
+import java.sql.PreparedStatement;
+
+import java.sql.Connection;
+
 
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		//getConnection();
+		createTable();
 	}
+	
+	
+	public static void createTable() throws Exception{
+		try {
+			Connection con = getConnection();
+			PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS tablename(id int NOT NULL AUTO_INCREMENT, first varchar(255), last varchar(255), PRIMARY KEY(id))");
+			create.executeUpdate();
+		} catch(Exception e){
+			System.out.println(e);
+		}
+		finally{
+			System.out.println("Function complete");
+		}
+		
+	}
+
+	
+	
+	
+	
+	
 	public static Connection getConnection() throws Exception {
 		
 		try {
@@ -18,7 +43,7 @@ public class Main {
 			String password = "336699888";
 			Class.forName(driver);
 			
-			Connection conn = (Connection) DriverManager.getConnection(url,username,password);
+			Connection conn = DriverManager.getConnection(url,username,password);
 			System.out.println("Connected");
 			return conn;
 		} catch(Exception e){
